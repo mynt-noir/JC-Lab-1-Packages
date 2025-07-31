22b927e39d0205cf21a10aa3bb851af317fc83ac
mynt-noir <nuanpatricia.serrano@gcash.com>
2025-07-31 13:52:46 +0800

    feat: add packages and imports

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,19 @@
+package org.example;
+
+import org.example.packageone.ClassOne;
+import org.example.packagetwo.ClassTwo;
+import org.example.packagethree.ClassThree;
+import static java.lang.System.out;
+
+public class Main {
+    public static void main(String[] args) {
+        ClassOne classOne = new ClassOne();
+        ClassTwo classTwo = new ClassTwo();
+        ClassThree classThree = new ClassThree();
+
+        out.println(classOne.greet());
+        out.println(classTwo.greet());
+        out.println(classThree.greet());
+
+    }
+}
